0456716c2ce4cc822b39e010c82ac69f6fc7e65c
Lee <zxcv920729@gmail.com>
2026-04-18 18:15:19 +0800

    init commit

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+// 引入 SCSS 樣式（Bootstrap + 你的自訂樣式都在這裡）
+import '../css/styles.scss';
+
+// 引入 Bootstrap JS
+import * as bootstrap from 'bootstrap'
---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+    root: path.resolve(__dirname, './'),
+    build: {
+        outDir: '../dist'
+    },
+    server: {
+        port: 5173,
+        hot: true
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                silenceDeprecations: [
+                    'import',
+                    'mixed-decls',
+                    'color-functions',
+                    'global-builtin',
+                    'if-function',
+                ],
+            }
+        }
+    }
+});
